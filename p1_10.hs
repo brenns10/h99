2b8744e8baca3d1eb637692298caf87db4b370cd
Stephen Brennan <smb196@case.edu>
2015-06-25 15:32:25 -0400

    Solution for Problem 2.

--- p1_10.hs
+++ p1_10.hs
@@ -8,3 +8,13 @@ myLast :: [a] -> a
 myLast [] = error "list is empty"
 myLast [x] = x
 myLast (_:xs) = myLast xs
+
+-- Problem 2: Find the last but one element of a list.
+--- Solution 1: the probably expected solution, by cases
+myButLast :: [a] -> a
+myButLast [] = error "list is empty"
+myButLast [x] = error "list has just one element"
+myButLast [x,y] = x
+myButLast (_:xs) = myButLast xs
+--- Solution 2: by chaining (inspired by similar solution for Problem 1)
+myButLast' = head . tail . reverse

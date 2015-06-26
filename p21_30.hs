c98d4c05372f0a2d7fdf59fc42744bfff8dcba1d
Stephen Brennan <smb196@case.edu>
2015-06-26 13:58:04 -0400

    Solutions to Problem 22.

--- p21_30.hs
+++ p21_30.hs
@@ -7,3 +7,14 @@
 insertAt            :: a -> [a] -> Int -> [a]
 insertAt a l 1      = a:l
 insertAt a (x:xs) n = x:insertAt a xs (n-1)
+
+-- Problem 22: Create a list containing a range.
+--- Solution 1: List comprehension... duh.
+range     :: (Enum a, Ord a, Eq a) => a -> a -> [a]
+range x y = [x..y]
+--- Solution 2: Recursive.
+range'          :: (Enum a, Ord a, Eq a) => a -> a -> [a]
+range' x y
+    | x > y     = []
+    | x == y    = [y]
+    | otherwise = x:range' (succ x) y

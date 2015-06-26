38d7183f3cde5b824f7811f1ca193b7b9874319f
Stephen Brennan <smb196@case.edu>
2015-06-26 12:56:38 -0400

    Solution to Problem 17 - take+drop.

--- p11_20.hs
+++ p11_20.hs
@@ -43,9 +43,13 @@ dropEvery :: [a] -> Int -> [a]
 dropEvery l n = map snd $ filter (\(m,x) -> m/=n) $ zip (cycle [1..n]) l
 
 -- Problem 17: Split a list into two parts, where length of first part is given.
+--- Solution 1: Recursive
 split :: [a] -> Int -> ([a], [a])
 split [] _ = ([], [])
 split l n
     | n <= 0 = ([], l)
 split (x:xs) n = ((x:h),r)
     where (h,r) = split xs (n-1)
+--- Solution 2: take+drop
+split' :: [a] -> Int -> ([a], [a])
+split' l n = (take n l, drop n l)

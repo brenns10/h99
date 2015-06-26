96e91072504b281d00d132dbb173721a87337f1b
Stephen Brennan <smb196@case.edu>
2015-06-26 12:08:50 -0400

    Solution to problem 12. (although, apparently concatMap is a thing.)

--- p11_20.hs
+++ p11_20.hs
@@ -3,10 +3,17 @@
 -- Solutions by Stephen Brennan
 -- Problems from: https://wiki.haskell.org/99_questions/11_to_20
 
--- Problem 11: Modified run-length encoding.
+-- Problem 11: Modified run-length encoding. (if there is only one instance,
+-- just include the character, not the count)
 data RunLength a = Single a | Multiple Int a deriving Show
 encodeModified :: (Eq a) => [a] -> [RunLength a]
 encodeModified l@(x:_) = (if length bin == 1 then Single x
                           else Multiple (length bin) x):encodeModified rem
     where (bin,rem) = span (== x) l
 encodeModified [] = []
+
+-- Problem 12: Decode modified run-length encoding from problem 11.
+decodeModified :: [RunLength a] -> [a]
+decodeModified l = concat $ map decodeChar l
+    where decodeChar (Single x) = [x]
+          decodeChar (Multiple n x) = take n (repeat x)
